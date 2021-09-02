367924801aec502095adfcb142b442408e8818f1
lijiang <xmlijiang@gmail.com>
2021-09-02 11:11:23 +0800

    spring源码编译学习

---
+++ spring-aop-demo/src/main/java/com/jali/MyBeanFactoryPostProcessor.java
@@ -0,0 +1,12 @@
+package com.jali;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+public class MyBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+	@Override
+	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+		System.out.println("设置beanDefinition");
+	}
+}
---
+++ spring-aop-demo/src/test/java/Client.java
@@ -0,0 +1,12 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Client {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Object user = context.getBean("user");
+		System.out.println(user);
+
+	}
+}
